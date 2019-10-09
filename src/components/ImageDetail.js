6c61638fad97a4f3acb221769623780d44d832d2
Stefan SCHOUTEDEN <stefan.schouteden@un.org>
2019-10-09 17:31:32 +0200

    Second Commit

--- src/components/ImageDetail.js
+++ src/components/ImageDetail.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Text, StyleSheet, View, Image } from 'react-native';
 
 const ImageDetail = (props) => {
-    console.log(props);
+    //console.log(props);
     return (
         <View>
             <Image source={props.myImageSource}/>
