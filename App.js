e76d818ebed6d9640efa631ea897f1aa2c07d838
stefanvanbree <stefan.schouteden@gmail.com>
2019-10-11 19:50:35 +0200

    Include Color Screen

--- App.js
+++ App.js
@@ -3,13 +3,18 @@ import HomeScreen from './src/screens/HomeScreen';
 import ComponentsScreen from './src/screens/ComponentsScreen'
 import ListScreen from './src/screens/ListScreen'
 import ImageScreen from "./src/screens/ImageScreen";
+import CounterScreen from "./src/screens/CounterScreen";
+import ColorScreen from "./src/screens/ColorScreen";
 
 const navigator = createStackNavigator(
   {
-    Home: HomeScreen,
-    Components: ComponentsScreen,
-    List: ListScreen,
-    Image: ImageScreen
+        Home: HomeScreen,
+        Components: ComponentsScreen,
+        List: ListScreen,
+        Image: ImageScreen,
+        Counter: CounterScreen,
+        Color: ColorScreen
+
   },
   {
     initialRouteName: 'Home',
