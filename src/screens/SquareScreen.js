c16f9532c726ce99f7d2c4e1b0d1a811ca8231e4
stefanvanbree <stefan.schouteden@gmail.com>
2019-10-12 13:42:16 +0200

    Minimizing the size of the code

--- src/screens/SquareScreen.js
+++ src/screens/SquareScreen.js
@@ -4,25 +4,37 @@ import ColorCounter from '../components/ColorCounter';
 
 const COLOR_INCREMENT=15;
 
+
+
 const SquareScreen = () => {
     const [red, setRed] = useState(0);
     const [green, setGreen] = useState(0);
     const [blue, setBlue] = useState(0);
 
-    //console.log(red);
-    //console.log(green);
-    //console.log(blue);
+    const setColor = (color, change) => {
+        //color==='red', 'green', 'blue'
+        //change = +15, -15
+        switch (color) {
+            case 'red' : red + change > 255 || red + change < 0 ? null : setRed(red + change);
+            return;
+            case 'green' : green + change > 255 || green + change < 0 ? null : setGreen(green + change);
+            return;
+            case 'blue' : blue + change > 255 || blue + change < 0 ? null : setBlue(blue + change);
+            return;
+            }
+        }
 
     return (
-    <View>
-        <ColorCounter color="Red" onIncrease={() => setRed(red + COLOR_INCREMENT)}
-                      onDecrease={() => setRed(red - COLOR_INCREMENT)}/>
-        <ColorCounter color="Green" onIncrease={() => setGreen(green + COLOR_INCREMENT)}
-                      onDecrease={() => setGreen(green - COLOR_INCREMENT)}/>
-        <ColorCounter color="Blue" onIncrease={() => setBlue(blue + COLOR_INCREMENT)}
-                      onDecrease={() => setBlue(blue - COLOR_INCREMENT)}/>
-        <View style={{height: 150, width: 150, backgroundColor: `rgb(${red}, ${green}, ${blue})`}}/>
-    </View>
+        <View>
+            <ColorCounter color="Red" onIncrease={() => setColor('red', COLOR_INCREMENT)}
+                          onDecrease={() =>  setColor('red', -1 * COLOR_INCREMENT)}/>
+            <ColorCounter color="Green" onIncrease={() => setColor('green', COLOR_INCREMENT)}
+                          onDecrease={() =>  setColor('green', -1 * COLOR_INCREMENT)}/>
+            <ColorCounter color="Blue" onIncrease={() => setColor('blue', COLOR_INCREMENT)}
+                          onDecrease={() =>  setColor('blue', -1 * COLOR_INCREMENT)}/>
+            <View style={{height: 150, width: 150, backgroundColor: `rgb(${red}, ${green}, ${blue})`}}/>
+            <Text>RGB Values: R: {red}, G: {green}, B: {blue}</Text>
+        </View>
 
     );
 };
