816649b5c183f0a87522068c166541adb4cd40cb
Stefan SCHOUTEDEN <stefan.schouteden@un.org>
2019-10-14 09:46:43 +0200

    New Screen : Text Screen, initial commit

---
+++ src/screens/TextScreen.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+const TextScreen = () => {
+
+    return (
+        <View>
+            <Text>Text Screen</Text>
+        </View>
+        );
+}
+
+const styles = StyleSheet.create({
+    textStyle: {
+        marginVertical:20
+    }
+})
+
+export default TextScreen
